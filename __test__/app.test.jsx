78ff455dee440e988dba48f0beb6df6e8edff18c
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2023-10-26 17:27:37 -0500

    Remove Unnecessary imports

--- __test__/app.test.jsx
+++ __test__/app.test.jsx
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-// import { expect, describe, it } from 'jest';
 import App from '../src/client/app';
-import Sidebar from '../src/client/components/sidebar';
+
 
 
 describe('App tests', () => {
--- __test__/sidebar.test.jsx
+++ __test__/sidebar.test.jsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-// import { expect, describe, it } from 'jest';
 import Sidebar from '../src/client/components/sidebar';
 
 describe('sidebar tests', () => {
