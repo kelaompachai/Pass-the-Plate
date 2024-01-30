debba19e0d73fbd93b1333a9d918b4fa5da1b9ab
Joseph Paul <joepubacc@gmail.com>
2024-01-30 10:01:52 -0500

    include comment in models file that describes database schema

--- server/models/foodbankModel.js
+++ server/models/foodbankModel.js
@@ -9,6 +9,30 @@ const pool = new Pool({
   connectionString: PG_URI,
 });
 
+/* Database Schema
+
+3 tables: comment, listing, users
+
+users table:
+  username varchar(20) not null and unique
+  password varchar (20) not null
+  zip code varchar (5)
+  id serial primary key
+
+listing table:
+  title varchar(20) not null
+  listing_body varchar(100) not null
+  zipcode varchar(5) not null
+  id serial primary key
+  user_id int foreign key that links to user(id)
+
+comment table:
+  comment_body varchar(100) not null
+  id serial primary key
+  listing_id int foreign key that links to listing(id)
+
+*/
+
 module.exports = {
   query: (text, params, callback) => {
     console.log('querying database with: ', text);
