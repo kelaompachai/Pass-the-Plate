4605847052990cfed6e8cbe4bbdcd84d651880f9
Joseph Paul <joepubacc@gmail.com>
2024-01-30 10:45:19 -0500

    mostly fix frontend app test

--- __test__/app.test.jsx
+++ __test__/app.test.jsx
@@ -2,11 +2,22 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import App from '../src/client/app';
 
+const mockData = [{
+  hi: 'hello'
+}];
+
+global.fetch = jest.fn(() => {
+  return Promise.resolve({
+    json: () => Promise.resolve(mockData)
+  })
+});
+
+
 
 
 describe('App tests', () => {
   it('should contains the heading Pass The Plate', () => {
-    render(<App />);
+    render(<App />)
     const heading = screen.getByText(/Pass the Plate/i);
     expect(heading).toBeInTheDocument();
   });
